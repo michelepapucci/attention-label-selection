a9b7415c855d962d15c1dc75316a5f78a78d07d0
Michele Papucci <m.papucci@studenti.unipi.it>
2024-01-15 17:57:13 +0100

    fix

--- remove_tech.py
+++ remove_tech.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 if __name__ == "__main__":
     df = pd.read_csv("data/training_filtered.csv")
-    df = df[df["Topic"] != "Technology"]
-    df.to_csv("data/training_filtered_no_tech.csv", index=False)
-    df = pd.read_csv("data/training_filtered.csv")
-    df = df[df["Topic"] != "Technology"]
-    df.to_csv("data/test_1_filtered_no_tech.csv", index=False)
+    new_df = df = df.drop(df[df["Topic"] == "TECHNOLOGY"].index)
+    new_df.to_csv("data/training_filtered_no_tech.csv", index=False)
+    df = pd.read_csv("data/test_1_filtered.csv")
+    new_df = df.drop(df[df["Topic"] == "TECHNOLOGY"].index)
+    new_df.to_csv("data/test_1_filtered_no_tech.csv", index=False)
